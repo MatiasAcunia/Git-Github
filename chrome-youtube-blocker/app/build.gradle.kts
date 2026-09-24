1db780784947225ce0dc696318c03e2ab4a21595
Matías Acuña <wollsyt@gmail.com>
2026-09-24 18:20:07 -0300

    Version 1.4 reactivation UI

--- chrome-youtube-blocker/app/build.gradle.kts
+++ chrome-youtube-blocker/app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.naini.chromeyoutubeblocker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
